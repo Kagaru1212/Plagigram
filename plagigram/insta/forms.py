0573a35e7f517507fbef21600112606a3dfde485
Oleksandr Pinchuk <@foxstudent106121>
2023-12-26 21:49:46 +0300

    commit 01

---
+++ plagigram/insta/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Post, UploadImage
+
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'user', 'tags']
+
+
+class UploadImageForm(forms.ModelForm):
+    class Meta:
+        model = UploadImage
+        fields = ['image']
+
+
+PostUploadImageFormSet = inlineformset_factory(Post, UploadImage, fields=('image',), extra=1, can_delete=True)
--- plagigram/insta/urls.py
+++ plagigram/insta/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),  # http://localhost:8000/plagigram/
+    path('addpost/', views.add_post, name='addpost'),  # http://localhost:8000/plagigram/addpost/
 ]
