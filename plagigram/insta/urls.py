7507debf920c05604269bda954aa4689256f4cd9
Oleksandr Pinchuk <@foxstudent106121>
2024-01-08 16:02:25 +0300

    commit 02

--- plagigram/insta/urls.py
+++ plagigram/insta/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),  # http://localhost:8000/plagigram/
     path('addpost/', views.add_post, name='addpost'),  # http://localhost:8000/plagigram/addpost/
+    path('post/<int:post_id>/', views.post_comments, name='comments'),
+    path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
