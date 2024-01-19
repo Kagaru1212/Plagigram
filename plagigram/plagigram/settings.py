0cf7316a50a94fa6829e0a1f94fce605ac796904
Oleksandr Pinchuk <@foxstudent106121>
2024-01-19 22:52:07 +0300

    commit deploy1

--- plagigram/plagigram/settings.py
+++ plagigram/plagigram/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
+ALLOWED_HOSTS = ['*']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ENVIRONMENT = os.environ.get("ENV", "PROD")
